c1993087c72b5fac9d34db06f81a91775a898633
sgc0907 <sgc0907@naver.com>
2019-05-06 18:46:49 +0900

    Create InputPrint, OutputPrint function

--- ColorCodeTranslator.c
+++ ColorCodeTranslator.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
-//test1
+int InputPrint (char *Hcode) {
+	printf("Input Yout Hexadecimal ColorCode (XXXXXX) : ");
+
+	for (int i = 0; i < 6; i++)
+		scanf(" %c", &Hcode[i]);
+
+	printf("\n");
+
+}
+
+int OutPutPrint (int *Dcode) {
+	printf("Decimal ColorCode : (");
+
+	for (int i = 0; i < 3; i++) {
+		printf("%d", Dcode[i]);
+
+		if (i != 2)
+			printf(",");
+	}
+
+	printf(") \n\n");
+	printf("==================================\n\n");
+}
 
 int main () {
 
